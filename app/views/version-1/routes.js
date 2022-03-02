75289cfdb678dd0740eb52a5e6a6acbfb9c6c44c
JayKAujla <jaypreet.aujla1@justice.gov.uk>
2022-03-02 13:54:35 +0000

    updated gov pay link

--- app/views/version-1/routes.js
+++ app/views/version-1/routes.js
@@ -3099,7 +3099,7 @@ module.exports = (router) => {
     if (req.body['submit-button'] === 'save-and-continue') {
       if (errors.length === 0) {
 //        req.session.data.reviewStatus = 'completed'
-        res.redirect('https://products.payments.service.gov.uk/pay/02133e4814ea416cb7a1e540b49a8545')
+        res.redirect('https://products.payments.service.gov.uk/pay/ba256382da5b435a81583a63b3c07375')
       }
       else {
         res.render('.//version-1/check-pay-and-submit/declaration', { errors: errors })
